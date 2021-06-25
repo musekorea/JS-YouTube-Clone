2ce9dd7a80739d485c6a3965787cdb68bbae1f4a
m3use <77679025+musekorea@users.noreply.github.com>
2021-06-25 20:44:03 +0800

    session&cookie

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import session from 'express-session';
 import rootRouter from './routers/rootRouter';
 import userRouter from './routers/userRouters';
 import videoRouter from './routers/videoRouters';
@@ -10,6 +11,21 @@ app.use(morgan('dev'));
 app.set('view engine', 'pug');
 app.set('views', process.cwd() + '/src/views');
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: 'moya',
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use((req, res, next) => {
+  console.log(`cookie=`, req.headers.cookie);
+  next();
+});
+app.use((req, res, next) => {
+  console.log(`sessionID`, req.session, req.sessionID, req.session.id);
+  next();
+});
 
 app.use('/', rootRouter);
 app.use('/videos', videoRouter);
