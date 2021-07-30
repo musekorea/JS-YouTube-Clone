40e20746038685e2799a76c761f3aa9ecea660fb
m3use <77679025+musekorea@users.noreply.github.com>
2021-07-30 18:47:31 +0800

    player play pause

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,21 @@
-console.log('This is Video Player');
+const video = document.querySelector('video');
+const play = document.querySelector('#playerPlayBtn');
+const mute = document.querySelector('#playerMuteBtn');
+const time = document.querySelector('#playerTimeSpan');
+const volume = document.querySelector('#playerVolumeRange');
+console.log(video, play, mute, time, volume);
+
+const handlePlay = (e) => {
+  play.classList;
+  if (video.paused) {
+    video.play();
+    play.className = `fas fa-pause`;
+  } else {
+    video.pause();
+    play.className = `fas fa-play`;
+  }
+};
+const handleMute = (e) => {};
+
+play.addEventListener('click', handlePlay);
+mute.addEventListener('click', handleMute);
