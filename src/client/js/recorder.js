1a3ba0cfe67859a4b0c96e8b3b8749d73cb083eb
m3use <77679025+musekorea@users.noreply.github.com>
2021-08-04 00:49:27 +0800

    recorder preview

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,11 +1,15 @@
 const recStartBtn = document.querySelector('.videoRecorderContainer');
+const preview = document.querySelector('#preview');
+console.log(preview);
 
 const handleStart = async () => {
   let stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
-    video: true,
+    video: { width: 400, height: 240 },
   });
   console.log(stream);
+  preview.srcObject = stream;
+  preview.play();
 };
 
 recStartBtn.addEventListener('click', handleStart);
