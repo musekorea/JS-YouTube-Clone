3c7414970e41ca60449d3fe6640a984562e738e9
m3use <77679025+musekorea@users.noreply.github.com>
2021-08-07 18:42:34 +0800

    clean code

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,6 +1,6 @@
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 
-const recStartBtn = document.querySelector('.videoRecorderContainer');
+const actionBtn = document.querySelector('.videoRecorderContainer');
 const preview = document.querySelector('#preview');
 const readySpan = document.querySelector('#startBtn');
 const startSpan = document.querySelector('#recBtn');
@@ -13,8 +13,8 @@ let videoURL;
 
 const handleReady = async () => {
   preview.classList.remove('hide');
-  recStartBtn.classList.add('active');
-  recStartBtn.style.backgroundColor = `red`;
+  actionBtn.classList.add('active');
+  actionBtn.style.backgroundColor = `red`;
 
   stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
@@ -24,8 +24,8 @@ const handleReady = async () => {
     console.log(stream);
     readySpan.classList.add('hide');
     startSpan.classList.remove('hide');
-    recStartBtn.removeEventListener('click', handleReady);
-    recStartBtn.addEventListener('click', handleStartRecord);
+    actionBtn.removeEventListener('click', handleReady);
+    actionBtn.addEventListener('click', handleStartRecord);
     preview.srcObject = stream;
     preview.play();
   }
@@ -35,11 +35,11 @@ const handleStartRecord = () => {
   console.log('start record');
   startSpan.classList.add('hide');
   stopSpan.classList.remove('hide');
-  recStartBtn.style.backgroundColor = `green`;
-  recStartBtn.classList.remove('active');
+  actionBtn.style.backgroundColor = `green`;
+  actionBtn.classList.remove('active');
   preview.classList.add('recording');
-  recStartBtn.removeEventListener('click', handleStartRecord);
-  recStartBtn.addEventListener('click', handleStopRecord);
+  actionBtn.removeEventListener('click', handleStartRecord);
+  actionBtn.addEventListener('click', handleStopRecord);
   recorder = new MediaRecorder(stream, { mimeType: 'video/webm' });
   recorder.start();
 };
@@ -47,8 +47,8 @@ const handleStartRecord = () => {
 const handleStopRecord = () => {
   console.log('stop record');
   stopSpan.classList.add('hide');
-  recStartBtn.style.backgroundColor = `steelblue`;
-  recStartBtn.removeEventListener('click', handleStopRecord);
+  actionBtn.style.backgroundColor = `steelblue`;
+  actionBtn.removeEventListener('click', handleStopRecord);
   recorder.stop();
   recorder.ondataavailable = (e) => {
     videoURL = URL.createObjectURL(e.data);
@@ -60,47 +60,50 @@ const handleStopRecord = () => {
   };
 };
 
+const files = {
+  input: 'recording.webm',
+  output: 'output.mp4',
+  thumb: 'thumbnail.jpg',
+};
+
+const downloadFile = (fileURL, fileName) => {
+  const a = document.createElement('a');
+  a.href = fileURL;
+  a.download = fileName;
+  document.body.appendChild(a);
+  a.click();
+};
+
 const handleDownload = async () => {
   const ffmpeg = createFFmpeg({ log: true });
   await ffmpeg.load();
-  ffmpeg.FS('writeFile', 'recording.webm', await fetchFile(videoURL));
-  await ffmpeg.run('-i', 'recording.webm', '-r', '60', 'output.mp4');
+  ffmpeg.FS('writeFile', files.input, await fetchFile(videoURL));
+  await ffmpeg.run('-i', files.input, '-r', '60', files.output);
   await ffmpeg.run(
     '-i',
-    'recording.webm',
+    files.input,
     '-ss',
     '00:00:01',
     '-frames:v',
     '1',
-    'thumbnail.jpg'
+    files.thumb
   );
-  const mp4File = ffmpeg.FS('readFile', 'output.mp4');
-  const thumbFile = ffmpeg.FS('readFile', 'thumbnail.jpg');
+  const mp4File = ffmpeg.FS('readFile', files.output);
+  const thumbFile = ffmpeg.FS('readFile', files.thumb);
   const mp4Blob = new Blob([mp4File.buffer], { type: 'video/mp4' });
   const thumbBlob = new Blob([thumbFile.buffer], { type: 'image/jpg' });
   const mp4URL = URL.createObjectURL(mp4Blob);
   const jpgURL = URL.createObjectURL(thumbBlob);
-  console.log(jpgURL);
 
-  const videoDownload = document.createElement('a');
   preview.classList.remove('recording');
   finalSpan.classList.remove('hide');
-  videoDownload.href = mp4URL;
-  videoDownload.download = 'MyRecording.mp4';
-  document.body.appendChild(videoDownload);
-  //videoDownload.innerHTML = 'ok';
-  videoDownload.click();
 
-  const thumbDownload = document.createElement('a');
-  thumbDownload.href = jpgURL;
-  thumbDownload.download = 'thumbnail.jpg';
-  document.body.appendChild(thumbDownload);
-  //thumbDownload.innerHTML = 'ok';
-  thumbDownload.click();
+  downloadFile(mp4URL, 'myRecording.mp4');
+  downloadFile(jpgURL, 'MyThumbnail.jpg');
 
-  ffmpeg.FS('unlink', 'recording.webm');
-  ffmpeg.FS('unlink', 'output.mp4');
-  ffmpeg.FS('unlink', 'thumbnail.jpg');
+  ffmpeg.FS('unlink', files.input);
+  ffmpeg.FS('unlink', files.output);
+  ffmpeg.FS('unlink', files.thumb);
 
   URL.revokeObjectURL(mp4URL);
   URL.revokeObjectURL(jpgURL);
@@ -111,4 +114,4 @@ const handleDownload = async () => {
   stream = null;
 };
 
-recStartBtn.addEventListener('click', handleReady);
+actionBtn.addEventListener('click', handleReady);
